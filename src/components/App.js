a2473957b67bebdc73627571930e7584c467286b
jpark799 <jason.j.park79@gmail.com>
2019-10-18 16:15:51 +0900

    lint

--- src/components/App.js
+++ src/components/App.js
@@ -30,22 +30,6 @@ const Gray = ({ children }) => (
   <span style={{ color: "#909090" }}>{children}</span>
 );
 
-// const Main = () => (
-//   <main>
-//     <Switch>
-//       <Route exact path='/' component={Form } render/>
-//       <Route path='/Dashboard' component={Dashboard}/>
-//     </Switch>
-//   </main>
-// )
-// const formComponent =  <Form setSeries={this.setSeries} setPieData={this.setPieData} />
-
-// const App = () => (
-//   <div>
-//     <Main/>
-//   </div>
-// )
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -54,17 +38,17 @@ class App extends Component {
       series: [
         {
           name: "US Workforce",
-          color: 'red',
+          color: "red",
           data: [78, 12.3, 6.3, 17.3]
         },
         {
           name: "US Software Engineers",
-          color: 'black',
+          color: "black",
           data: [58.6, 3.8, 35.4, 5.3]
         },
         {
           name: "Your Company",
-          color: 'green',
+          color: "green",
           // need to pass in companyRace here.
           data: []
         }
@@ -74,11 +58,6 @@ class App extends Component {
     };
   }
   // should be in Dashboard??
-
- 
-
-
-
 
   setSeries = companyRaceData => {
     //copy state
@@ -107,19 +86,8 @@ class App extends Component {
     this.setState(stateCopy);
   };
 
-  // onSubmit = fields => {
-  //   console.log("App Component received: ", fields);
-  //   let whiteTotal = parseInt(fields.whiteM)+parseInt(fields.whiteF)
-  //   let blackTotal = parseInt(fields.blackM)+parseInt(fields.blackF)
-  //   let asianTotal = parseInt(fields.asianM)+parseInt(fields.asianF)
-  //   let hispanicLatinTotal = parseInt(fields.hispanicLatinM)+parseInt(fields.hispanicLatinF)
-  //   let companyRace = [whiteTotal, blackTotal, asianTotal, hispanicLatinTotal]
-  //   console.log(companyRace)
-  // };
-
   render() {
-    
-// START
+    // START
     const optionsRace = {
       title: {
         text: "Race Data"
@@ -184,83 +152,74 @@ class App extends Component {
     };
 
     const pieOptionsUsSoftwareGender = {
-        chart: {
-          type: "pie"
-        },
-        title: {
-          text: "US Software Engineers"
-        },
-  
-        subtitle: {
-          text: "Source: US Burea of Labor"
-        },
-        legend: {
-          layout: "vertical",
-          align: "right",
-          verticalAlign: "middle",
-          borderWidth: 0
-        },
-        tooltip: {
-          valueSuffix: "%"
-        },
-        series: [
-          {
-            name: "US Labor Force",
-            keys: ["name", "y", "color", "label"],
-            data: [
-              ["Male", 80.7, "#0000FF", "Male"],
-              ["Female", 19.3, "#EB001F", "Female"]
-            ],
-            dataLabels: {
-              enabled: true,
-              format: "{point.label}"
-            }
+      chart: {
+        type: "pie"
+      },
+      title: {
+        text: "US Software Engineers"
+      },
+
+      subtitle: {
+        text: "Source: US Burea of Labor"
+      },
+      legend: {
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle",
+        borderWidth: 0
+      },
+      tooltip: {
+        valueSuffix: "%"
+      },
+      series: [
+        {
+          name: "US Labor Force",
+          keys: ["name", "y", "color", "label"],
+          data: [
+            ["Male", 80.7, "#0000FF", "Male"],
+            ["Female", 19.3, "#EB001F", "Female"]
+          ],
+          dataLabels: {
+            enabled: true,
+            format: "{point.label}"
           }
-        ]
-      };
+        }
+      ]
+    };
     const yourCompanyGender = {
-        chart: {
-          type: "pie"
-        },
-        title: {
-          text: "Your Company Data"
-        },
-  
-        subtitle: {
-          text: "Source: US Burea of Labor"
-        },
-        legend: {
-          layout: "vertical",
-          align: "right",
-          verticalAlign: "middle",
-          borderWidth: 0
-        },
-        tooltip: {
-          valueSuffix: "%"
-        },
-        series: [
-          {
-            name: "US Labor Force",
-            keys: ["name", "y", "color", "label"],
-            data: this.props.pieData
-            ,
-            dataLabels: {
-              enabled: true,
-              format: "{point.label}"
-            }
+      chart: {
+        type: "pie"
+      },
+      title: {
+        text: "Your Company Data"
+      },
+
+      subtitle: {
+        text: "Source: US Burea of Labor"
+      },
+      legend: {
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle",
+        borderWidth: 0
+      },
+      tooltip: {
+        valueSuffix: "%"
+      },
+      series: [
+        {
+          name: "US Labor Force",
+          keys: ["name", "y", "color", "label"],
+          data: this.props.pieData,
+          dataLabels: {
+            enabled: true,
+            format: "{point.label}"
           }
-        ]
-      };
-
-      // FIN
-
-
-
-
+        }
+      ]
+    };
 
     return (
-      
-
       <BrowserRouter>
         <Switch>
           <Parallax
@@ -288,14 +247,7 @@ class App extends Component {
               offset={4}
               speed={1}
               style={{ backgroundColor: "#87BCDE" }}
-              
             />
-
-            {/* <Parallax.Layer
-              offset={5}
-              speed={1}
-              style={{ backgroundColor: "#87BCDE" }}
-            /> */}
 
             <Parallax.Layer
               offset={0}
@@ -416,14 +368,12 @@ class App extends Component {
               <img src={url("earth")} style={{ width: "10%" }} />
             </Parallax.Layer>
 
-   
-
             <Parallax.Layer
               offset={4}
               speed={-0.3}
               style={{
                 backgroundSize: "30.5%",
-                backgroundPosition: "center",
+                backgroundPosition: "center"
                 // backgroundImage: url("clients-main", true)
               }}
             />
@@ -488,21 +438,20 @@ class App extends Component {
                 }}
               >
                 <h1>The Numbers Don't Lie</h1> <br />
-                <br/>
+                <br />
                 A diversity of informed views enables objections and <br />
                 alternatives to be explored more efficiently. <br />
                 Solutions are able to emerge more readily <br />
-                and are adopted with greater confidence. <br /> 
+                and are adopted with greater confidence. <br />
                 <br />
                 Companies that were in the top quartile for gender <br />
                 diversity were 15% more likely to have financial returns <br />
-                that were above their national industry medians and <br /> 
+                that were above their national industry medians and <br />
                 companies that were in the top quartile for racial <br />
-                diversity were 35% more likely. <br/> 
-                <i>Source: Diversity Matters (2015) - McKinsey & Company</i><br/>
-
+                diversity were 35% more likely. <br />
+                <i>Source: Diversity Matters (2015) - McKinsey & Company</i>
+                <br />
               </div>
-
             </Parallax.Layer>
 
             <Parallax.Layer
@@ -515,10 +464,6 @@ class App extends Component {
                 justifyContent: "center"
               }}
             >
-              {/* <img
-                        src={url('bash')}
-                        style={{ width: '40%', transform: [{ scale: this.animation }] }}
-                    /> */}
               <div
                 style={{
                   whiteSpace: "pre",
@@ -531,24 +476,19 @@ class App extends Component {
                 <h1>Diversity Drives Innovation</h1> <br />
                 <br />
                 Roughly half of the world's population is female <br />
-                while over 80% of US software engineers are male.  <br /> <br />
-
-                As technology continues to play an increasingly <br/>
-                integral role in our daily lives, it's more  <br/>
-                important than ever to ensure that the demographics <br/> 
-                of the tech industry reflect the demographics of <br/>
-                society so that they can accurately build for<br/>
-                the needs of society. <br/> <br/>
-
-                Imagine all of the great ideas and innovations we <br/>
-                could be missing out on because of a lack of  <br/>
+                while over 80% of US software engineers are male. <br /> <br />
+                As technology continues to play an increasingly <br />
+                integral role in our daily lives, it's more <br />
+                important than ever to ensure that the demographics <br />
+                of the tech industry reflect the demographics of <br />
+                society so that they can accurately build for
+                <br />
+                the needs of society. <br /> <br />
+                Imagine all of the great ideas and innovations we <br />
+                could be missing out on because of a lack of <br />
                 representation.
-
-                
-
               </div>
               {/* <HighchartsReact highcharts={Highcharts} options={pieOptionsUsGender} /> */}
-
             </Parallax.Layer>
 
             <Parallax.Layer
@@ -579,12 +519,11 @@ class App extends Component {
                 Data serves as an important catalyst <br />
                 for change and progress while also providing <br />
                 greater insight into hiring, progression, <br />
-                and retention within organizations. <br/> <br/>
+                and retention within organizations. <br /> <br />
                 Less than 20% of Fortune 500 companies <br />
                 publish annual diversity reports <br />
-                while 78% of the US work force list Diversity  <br />
+                while 78% of the US work force list Diversity <br />
                 & Inclusion as extremely important. <br /> <br />
-
               </div>
             </Parallax.Layer>
 
@@ -600,65 +539,32 @@ class App extends Component {
                 path="/"
                 render={routeProps => (
                   <Form
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center"
-                  }}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}
                     setSeries={this.setSeries}
                     setPieData={this.setPieData}
                     routeProps={routeProps}
                   />
                 )}
               />
-              
-          <Route
-          offset={5}
-            path="/dash"
-            render={() => (
 
-              
-              <Dashboard
-                series={this.state.series}
-                pieData={this.state.pieData}
-              />
-              
-            )}
-          />
-            </Parallax.Layer>
-
-            {/* <Parallax.Layer
-              offset={5}
-              speed={-0}
-              // style={{
-              //   display: "flex",
-              //   alignItems: "center",
-              //   justifyContent: "center"
-              // }}
-              // Reset Scroll
-              // onClick={() => this.parallax.scrollTo(0)}
-            >
-              {/* <Route
+              <Route
+                offset={5}
                 path="/dash"
                 render={() => (
                   <Dashboard
                     series={this.state.series}
                     pieData={this.state.pieData}
                   />
-                )} */}
-
-                    </Parallax>
-
+                )}
+              />
+            </Parallax.Layer>
+          </Parallax>
         </Switch>
       </BrowserRouter>
-          //{" "}
-          // <div className="App">
-          //   // <Form setSeries={this.setSeries} setPieData={this.setPieData} />
-          //   // <Dashboard series={this.state.series} />
-          //   // {/* <PieChart data={this.state.pieData} /> */}
-          //   //{" "}
-          // </div>
-          
     );
   }
 }
